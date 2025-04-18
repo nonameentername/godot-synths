7e6db4a8e160884d1b35bfcc93dc56e2840267a4
werner mendizabal <nonameentername@gmail.com>
2025-04-17 19:35:51 -0500

    Update to fix loading preset settings for reverb

--- package.gd
+++ package.gd
@@ -2,5 +2,5 @@ extends "res://addons/godot-package/package.gd"
 
 
 func _requirements():
-	dependency("nonameentername/godot-csound", {"tag": "v0.1.0-beta.21"})
+	dependency("nonameentername/godot-csound", {"tag": "v0.1.0-beta.32"})
 	dependency("nonameentername/godot-jack", {"tag": "v0.1.0-beta.2"})
