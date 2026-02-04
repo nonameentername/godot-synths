83ec9bc14f7baa94d16e7f5d5065da35bd96c330
werner mendizabal <nonameentername@gmail.com>
2026-02-03 21:58:54 -0600

    Fix script error

--- package.gd
+++ package.gd
@@ -2,5 +2,5 @@ extends "res://addons/godot-package/package.gd"
 
 
 func _requirements():
-	dependency("nonameentername/godot-csound", {"tag": "v0.1.0-beta.120"})
+	dependency("nonameentername/godot-csound", {"tag": "v0.1.0-beta.121"})
 	dependency("nonameentername/godot-jack", {"tag": "v0.1.0-beta.2"})
